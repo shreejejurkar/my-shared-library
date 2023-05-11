acf3f2032addd0fd51d7ed5c1dc281a0e5b4b285
shreejejurkar <jejurkar.shriganesh@gmail.com>
2023-05-11 12:46:37 +0530

    udated dockerBuild

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,9 +1,9 @@
-def call(String project, String ImageTag, String hubUser){
+def call(String project, String ImageTag, String DockerHubUser){
     
     sh """
-     docker image build -t ${hubUser}/${project} . 
-     docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
-     docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+     docker image build -t ${DockerHubUser}/${project} . 
+     docker image tag ${DockerHubUser}/${project} ${DockerHubUser}/${project}:${ImageTag}
+     docker image tag ${DockerHubUser}/${project} ${DockerHubUser}/${project}:latest
     """
 }
 
